18cca75bf3532906e4218620255065a4f792ec6c
gcrespo17 <gcrespo1705@gmail.com>
2024-10-12 17:41:43 -0400

    curso sesion 132

--- angular-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ angular-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,27 +7,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit{
+export class ServerStatusComponent implements OnInit, AfterViewInit{
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  //Forma vieja de OnDestroy
+  // private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
 
-  // Esta opcion no es la recomendada
-  // constructor(){
-  //   setInterval(()=>{
-  //     const rnd=Math.random(); //0-0.999999999999
-
-  //     if(rnd<0.5){
-  //       this.currentStatus='online';
-  //     }else if(rnd<0.9){
-  //       this.currentStatus='offline';
-  //     }else{
-  //       this.currentStatus='unknown';
-  //     }
-  //   }, 5000);
-  // }
-
-  //Esta es la forma recomendada
   ngOnInit(){
-    setInterval(()=>{
+    console.log('ON INIT');
+    //this.interval=
+    const interval=setInterval(()=>{
       const rnd=Math.random(); //0-0.999999999999
 
       if(rnd<0.5){
@@ -38,7 +27,20 @@ export class ServerStatusComponent implements OnInit{
         this.currentStatus='unknown';
       }
     }, 5000);
+    //Esta es la forma elegante de destruir, pero solo disponible en versiones nuevas de Angular
+    this.destroyRef.onDestroy(()=>{
+      clearInterval(interval);
+    });
   }
 
+  ngAfterViewInit(){
+    console.log('AFTER VIEW INIT');
+  }
+  
+  //Destruye el intervalo que se creo arriba para que no existan memory leaks y sea mas eficienta
+  //Esta es la forma vieja
+  // ngOnDestroy(){
+  //   clearTimeout(this.interval);
+  // }
 
 }
--- angular-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ angular-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  //Me ayuda a encontrar elementos hijos en la vista de l componente
+  //Esto es para usar el elemto template de forma global
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>; 
+  ////////////////////////////////////////
+  //La forma de usarlo como signal es la siguiente
+  private form = viewChild<ElementRef<HTMLFormElement>>('form');
 
+  //Esto es una alternativa a two way binding
+  onSubmit(title: string, ticketText: string){
+    console.log(title);
+    console.log(ticketText);
+    // this.form?.nativeElement.reset();
+    /////////////////
+    //Como signal:
+    this.form.855555555555555558888888
+  }
 }
